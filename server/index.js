21fe019ac7cf76e3cbb62883d3a0a571631ac962
Kat <catherineitj28@gmail.com>
2022-09-24 19:12:19 +0300

    feat: add dotenv

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
